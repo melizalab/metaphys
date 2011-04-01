eddda5b7f1bc03bc6daa997279104f72f0654609
Dan Meliza <dmeliza@uchicago.edu>
2011-04-01 12:36:12 -0500

    PlayStimulus: control file is a playlist; supports resampling; etc

--- Protocols/PlayStimulus.m
+++ Protocols/PlayStimulus.m
@@ -1,7 +1,9 @@
 function varargout = PlayStimulus(action)
 %
 % PLAYSTIMULUS Protocol for acquiring data episodically; in each episode a
-% pre-loaded output waveform is sent to one of the instrument's inputs
+% pre-loaded output waveform is sent to one of the instrument's inputs. The
+% user can specify a single file with data in it, or a list of files which
+% will be presented in turn.
 %
 % fig = PLAYSTIMULUS('init')
 % PLAYSTIMULUS('start')
@@ -27,7 +29,7 @@ switch lower(action)
     case 'start'
         DeleteSubscriber('loop')
         StopDAQ
-        loadData;
+        loadStimfile;
         % Setup data handling
         SetDataStorage('memory')
         % Set system to repeat
@@ -38,7 +40,7 @@ switch lower(action)
     case 'record'
         DeleteSubscriber('loop')
         StopDAQ
-        loadData;
+        loadStimfile;
         % Setup data handling
         dsmode  = GetParam(me, 'data_mode');
         instr   = GetParam(me, 'instrument');
@@ -74,37 +76,49 @@ function out = me()
 out = mfilename;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% 2011.03.18 MK
-function [] = loadData()
-% Load data from the stimulus file, center and rescale, and store
+function [] = loadStimfile()
+% Load the stimulus file and check it for consistency
+SetParam(me, 'stim_list',{});
 stimfile        = GetParam(me, 'stimulus', 'value');
+if ~isempty(stimfile)
+    [fid,msg] = fopen(stimfile,'rt');
+    if fid < 0
+        DebugPrint(['Error opening ' file ': ' msg]);
+    else
+        stimvals = textscan(fid, '%s%f', 'CommentStyle', '%');
+        fclose(fid);
+        DebugPrint(sprintf('%s: stimuli=%d',stimfile,length(stimvals{1}))); 
+        SetParam(me, 'stim_list', stimvals);
+    end
+else
+    DebugPrint('No stimulus file to load');
+end
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function [stimdata] = loadData(stimfile, samplerate)
+% Load data from the stimulus file, center and rescale, and store
 maxposcur       = GetParam(me, 'max_pos_cur', 'value');
 maxnegcur       = GetParam(me, 'max_neg_cur', 'value');
 instr   = GetParam(me,'instrument');
 chan    = GetParam(me,'data_output');
 Fs      = GetChannelSampleRate(instr, chan) / 1000;
 % should check sampling rate
-if ~isempty(stimfile)
-  stimdata      = LoadStimulusWaveform(stimfile);
-  ind           = stimdata > 0.0;
-  stimdata(ind) = stimdata(ind) * abs(maxposcur);
-  stimdata(~ind) = stimdata(~ind) * abs(maxnegcur);
-  SetParam(me, 'stim_cache', stimdata);
-  DebugPrint('Loaded stimulus from %s, %d samples.', stimfile, ...
-             length(stimdata));
-  SweepDisplay('clearall', length(stimdata) / Fs);
-else
-  DebugPrint('No stimulus file to load');
-  SetParam(me, 'stim_cache', []);
-end
+stimdata      = LoadStimulusWaveform(stimfile, samplerate, Fs);
+ind           = stimdata > 0.0;
+stimdata(ind) = stimdata(ind) * abs(maxposcur);
+stimdata(~ind) = stimdata(~ind) * abs(maxnegcur);
+DebugPrint('Loaded stimulus from %s, %d samples.', stimfile, ...
+           length(stimdata));
+SweepDisplay('clearall', length(stimdata) / Fs);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [] = loopControl(packet)
 if strcmpi(packet.message.Type, 'Stop')
     ep_interval     = GetParam(me,'ep_interval','value');
+    stim_list       = GetParam(me,'stim_list');
     ep_repeats      = GetParam(me,'ep_repeats','value');
     current_sweep   = GetSweepCounter;
-    if current_sweep < ep_repeats
+    if (current_sweep / length(stim_list{1})) < ep_repeats;
         SweepPause(ep_interval);
         sweepControl
     else
@@ -117,13 +131,19 @@ function [] = sweepControl()
 % Queue command data
 instr       = GetParam(me,'instrument');
 chan        = GetParam(me,'data_output');
-stimdata    = GetParam(me,'stim_cache');
-if isempty(stimdata)
-  DebugPrint('No stimulus loaded');  % should pop up a dialog
+stimpath    = fileparts(GetParam(me, 'stimulus', 'value'));
+stim_list   = GetParam(me,'stim_list');
+ep_repeats  = GetParam(me,'ep_repeats','value');
+current_sweep   = GetSweepCounter;
+if isempty(stim_list)
+    DebugPrint('No stimuli');  % should pop up a dialog
 else
-  episodelength = PutInputData(instr, stimdata, {chan});
-  % Start a sweep
-  StartSweep(episodelength)
+    stim_num = mod(current_sweep, length(stim_list{1})) + 1;
+    stimdata = loadData(fullfile(stimpath,stim_list{1}{stim_num}),...
+        stim_list{2}(stim_num));
+    episodelength = PutInputData(instr, stimdata, {chan});
+    % Start a sweep
+    StartSweep(episodelength)
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- Utility/LoadStimulusWaveform.m
+++ Utility/LoadStimulusWaveform.m
@@ -1,16 +1,22 @@
-function [data] = LoadStimulusWaveform(stimfile, samplerate)
+function [data, Fs] = LoadStimulusWaveform(stimfile, samplerate, targetrate)
 % LOADSTIMULUSWAVEFORM Load a stimulus waveform from a file
 %
-% The stimulus can be stored in four formats: ASCII text, raw PCM,
-% WAV, or MAT.  For the first two cases, the data are assumed to be sampled at
-% the target rate.  For WAV files, if <samplerate> is defined the
-% data are resampled to match it, if necessary.  For a MAT file, the data
-% are assumed to be in a variable called 'data'
+% S,Fs = LOADSTIMULUSWAVEFORM(stimfile, samplerate, targetrate)
+%
+% Load sampled data from <stimfile>. Data can be stored in one of four
+% formats: ASCII text, raw PCM, WAV, or MAT.  WAV files contain information
+% about sampling rate; otherwise the <samplerate> argument can be used to
+% specify it.  Sampling rate only matters if the data need to be resampled
+% to <targetrate>.
+%
+% For a MAT file, the data are assumed to be in a variable called 'data'
 %
 % Copyright (C) 2011 Daniel Meliza <dmeliza@dylan.uchicago.edu>
 % Created 2011-02-22
 
-[pn,fn,ext] = fileparts(stimfile);
+error(nargchk(1,3,nargin));
+
+[~,~,ext] = fileparts(stimfile);
 
 switch lower(ext)
  case '.pcm'
@@ -18,14 +24,19 @@ switch lower(ext)
   data = fread(fp,inf,'int16');
   data = double(data) / 2^15;
  case '.wav'
-  [data,fs,bits] = wavread(stimfile);
+  [data,samplerate,bits] = wavread(stimfile);
   data = double(data) / 2^(bits-1);
-  if nargin > 1
-    warning('Resampling not yet implemented');
-  end
  case '.mat'
   z = load(stimfile,'-mat');
   data = z.data;
  otherwise
   data = load(stimfile,'-ascii');
 end
+
+if nargin > 2 && samplerate~=targetrate
+    data = resample(data, targetrate, samplerate);
+    Fs = targetrate;
+else
+    Fs = samplerate;
+end
+    
